daae7edaa6ba636fea2fbca8ad4b3df23e5d6847
Nicholas Salesky <nicksalesky@gmail.com>
2023-12-06 17:03:00 -0500

    Trying another attempt for day 5 part 2, doesn't work

--- day-05/src/part2.rs
+++ day-05/src/part2.rs
@@ -1,15 +1,9 @@
 use std::cmp::min;
+use std::collections::LinkedList;
+use std::ops::Range;
 use anyhow::Result;
 use itertools::Itertools;
-use rangemap::RangeMap;
-
-
-fn source_to_dest(source_num: u64, range_map: &RangeMap<u64, u64>) -> u64 {
-    match range_map.get_key_value(&source_num) {
-        Some((range, dest_start)) => dest_start + (source_num - range.start),
-        None => source_num,
-    }
-}
+use rangemap::{RangeMap, RangeSet};
 
 fn parse_section_ranges(section: &str) -> RangeMap<u64, u64> {
     section
@@ -26,15 +20,61 @@ fn parse_section_ranges(section: &str) -> RangeMap<u64, u64> {
         .collect()
 }
 
+fn process_ranges<T>(ranges: T, source_to_dest_map: &RangeMap<u64, u64>) -> Vec<Range<u64>>
+where
+T: IntoIterator<Item = Range<u64>>
+{
+    let mut results_set = RangeSet::new();
+
+    let mut worklist = LinkedList::from_iter(ranges);
+
+    while let Some(item_range) = worklist.pop_front() {
+        let first_item = item_range.start;
+        let last_item = item_range.end;
+        match source_to_dest_map.get_key_value(&first_item) {
+            Some((source_range, dest_start)) => {
+                let num_elements = min(
+                    last_item - first_item,
+                    last_item - source_range.start,
+                );
+                let num_remaining_elements = last_item - (first_item + num_elements);
+
+                let dest_range = *dest_start..(*dest_start + num_elements);
+
+                results_set.insert(dest_range);
+
+                if num_remaining_elements > 0 {
+                    worklist.push_back(first_item+num_elements..last_item);
+                }
+            },
+            None => {
+                results_set.insert(first_item..first_item+1);
+
+                if first_item + 1 < last_item {
+                    worklist.push_back(first_item+1..last_item);
+                }
+            }
+        };
+    }
+
+    results_set
+        .iter()
+        .map(|range| range.clone())
+        .collect()
+}
+
 pub fn process(input: &str) -> Result<String> {
     let sections: Vec<&str> = input.split("\n\n").collect();
     assert_eq!(sections.len(), 8);
 
-    let starting_seeds = sections[0]
+    let starting_seed_ranges: Vec<Range<u64>> = sections[0]
         .strip_prefix("seeds: ")
         .expect("first line lists the seeds")
         .split(' ')
-        .map(|seed| seed.parse::<u64>().expect("seed part is numeric"));
+        .map(|seed| seed.parse::<u64>().expect("seed part is numeric"))
+        .tuples::<(u64, u64)>()
+        .map(|(start, length)| start..start+length)
+        .collect();
 
 
     let seed_to_soil_map = parse_section_ranges(sections[1]);
@@ -45,20 +85,19 @@ pub fn process(input: &str) -> Result<String> {
     let temperature_to_humidity_map = parse_section_ranges(sections[6]);
     let humidity_to_location_map = parse_section_ranges(sections[7]);
 
-    let locations = starting_seeds
-        .tuples::<(u64, u64)>()
-        .flat_map(|(seed_start, range_length)| seed_start .. seed_start+range_length)
-        .map(|seed_num| {
-            let soil = source_to_dest(seed_num, &seed_to_soil_map);
-            let fertilizer = source_to_dest(soil, &soil_to_fertilizer_map);
-            let water = source_to_dest(fertilizer, &fertilizer_to_water_map);
-            let light = source_to_dest(water, &water_to_light_map);
-            let temperature = source_to_dest(light, &light_to_temperature_map);
-            let humidity = source_to_dest(temperature, &temperature_to_humidity_map);
-            source_to_dest(humidity, &humidity_to_location_map)
-        });
+    let soil_ranges = process_ranges(starting_seed_ranges, &seed_to_soil_map);
+    let fertilizer_ranges = process_ranges(soil_ranges, &soil_to_fertilizer_map);
+    let water_ranges = process_ranges(fertilizer_ranges, &fertilizer_to_water_map);
+    let light_ranges = process_ranges(water_ranges, &water_to_light_map);
+    let temperature_ranges = process_ranges(light_ranges, &light_to_temperature_map);
+    let humidity_ranges = process_ranges(temperature_ranges, &temperature_to_humidity_map);
+    let location_ranges = process_ranges(humidity_ranges, &humidity_to_location_map);
 
-    let min_location = locations.reduce(min).expect("there is at least one location");
+    let min_location = location_ranges
+        .iter()
+        .map(|range| range.start)
+        .reduce(min)
+        .expect("there is at least one location");
 
     Ok(min_location.to_string())
 }
